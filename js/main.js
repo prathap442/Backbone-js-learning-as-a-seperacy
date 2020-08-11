cbd8e90bb322078b2977b24788a292576e038217
prathap442 <pmohansaikrishna@gmail.com>
2020-08-11 16:50:22 +0530

    handling: the dom events
    - the dom events can be handled by writing the events into the events hash object
    - the events hash object has the following attributes like "click" and "click .bookmark"
    - e.stopPropation avoids the function to push the events to the lower elements .

--- js/main.js
+++ js/main.js
@@ -1,19 +1,50 @@
-var SongView = Backbone.View.extend({
-  tagName: "span",
-  className: "song",
-  id: "product",
+var AnimalView = Backbone.View.extend({
+  tagName: "li",
+  className: "document-row",
   attributes: {
-    'data-genre':'Jazz'
+    "data-genre": "Jazz"
   },
   //here this consists of the render method that is being defined would act as a reference when 
   //rendering the template. The template render should return the dom element at the end .
   render: function(){
-    //this.$el specifies the element of the jquery on the dom
-    this.$el.html("hello Sai This is prathap");
+    this.$el.append(this.model.get('name') + '<button>Button</button> <button class="bookmark">ButtonWithBook</button>');
     //this returning of the this statement is a must for the sake of hte continuous concatinatin that can occur now .
     return this;
+  },
+
+  //to handle out the events we use the events property to handle out the events on the dom .
+  events: {
+    //enable click event in general for the dom element
+    "click": "buttonClicked",
+    //enable click event for the elements with the class bookmark
+    "click .bookmark": "buttonClickBookMarkClass"
+  },
+  buttonClicked: function(){
+    console.log("The button is being clicked") 
+  },
+  //this function is to enable on click of the elements with the class buttonClickBookMarkClass
+  buttonClickBookMarkClass: function(e){
+    e.stopPropagation()
+    console.log("The click event for the class book mark is being made ")
   }
 });
 
-var songView = new SongView({el: '#container'})
-songView.render()
+//passing up of the data to the view using the Model
+
+var Animal = Backbone.Model.extend({
+  defaults: {
+    'animalType': 'mammal'
+  }
+  //here we can write the validate function if we want to validate specifically the models that we creaete
+})
+
+var animal1 = new Animal({'name': 'hog', 'size': 40, 'color': 'black'})
+var animal2 = new Animal({'name': 'Archers', 'size': 20, 'color': 'yellow'})
+//passsing the data onver here
+
+var animalView = new AnimalView({el: '#container', model: animal1})
+animalView.render()
+
+var animalCollection = Backbone.Collection.extend({
+  model: Animal
+})
